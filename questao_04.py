78cc588adf808ee7a6657b547552a51ea47e30ff
José David <josedaaaaavid@gmail.com>
2021-02-22 23:46:30 +0000

    Questões 1, 4, 5 e 7 prontas.

--- questao_04.py
+++ questao_04.py
@@ -1,1 +1,14 @@
 """ 4) Escreva um programa que pergunte a distância que um passageiro deseja percorrer em km. Calcule o preço da passagem, cobrando R$ 0,50 por km para viagens de até de 200 km, e R$ 0,45 para viagens mais longas. """
+
+print("Este programa vai calcular o preço da passagem de uma viagem de carro, custando cinquenta centavos para viagens de até duzentos quilômetros, e quarenta e cinco centavos para viagens mais longas, por quilômetro.")
+
+distancia = float(input("Qual a distância do trajeto em quilômetros? "))
+
+if distancia <= 200:
+  custoDoKm = 0.50
+else:
+  custoDoKm = 0.45
+
+preco = custoDoKm * distancia
+
+print(f"O preço da passagem é de R$ {preco:.2f}.")
--- questao_07.py
+++ questao_07.py
@@ -1,23 +1,24 @@
 """ 7) Escreva um programa que calcule o preço a pagar pelo fornecimento de energia elétrica. Pergunte a quantidade de kWh consumida e o tipo de instalação: R para residências, I para indústrias e C para comércios. Calcule o preço a pagar de acordo com a tabela a seguir. """
 
+print("A medida de kWh equivale à quantidade de kW consumidos a cada hora, e é igual também a 3600 quilojoules. Este utilitário vai calcular o preço a se pagar pelo consumo de energia elétrica de acordo com o tipo específico de residência na qual a corrente está sendo consumida.")
+
 kWh = int(input("Informe a quantidade de quilowatt-hora consumido(s): "))
 tipo = str(input("Informe agora o tipo de instalação (R para residências | I para indústrias | C para comércios): "))
 
 if tipo == "R":
   if kWh <= 500:
-    preco = 0.40
+    preco = 0.40 * kWh
   else:
-    preco = 0.65
+    preco = 0.65 * kWh
 elif tipo == "C":
   if kWh <= 1000:
-    preco = 0.55
+    preco = 0.55 * kWh
   else:
-    preco = 0.60
+    preco = 0.60 * kWh
 elif tipo == "I":
   if kWh <= 5000:
-    preco = 0.55
+    preco = 0.55 * kWh
   else:
-    preco = 0.60
-
+    preco = 0.60 * kWh
 
 print(f"O preço a se pagar será o equivalente ao valor de R$ {preco:.2f}.")
